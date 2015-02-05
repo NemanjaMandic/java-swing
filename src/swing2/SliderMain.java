0826141fb3750a32763c8e9ed93c1550e07e7ec5
Nemanja Mandic <nemanjans84@gmail.com>
2015-02-05 01:55:39 +0100

    JSlider, classes OvalPanel, SliderFrame and SliderMain

---
+++ src/swing2/SliderMain.java
@@ -0,0 +1,16 @@
+
+
+package swing2;
+
+import javax.swing.JFrame;
+
+
+public class SliderMain {
+    public static void main(String[] args){
+        SliderFrame sf = new SliderFrame();
+        sf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        sf.setLocationRelativeTo(null);
+        sf.setSize(220, 270);
+        sf.setVisible(true);
+    }
+}
